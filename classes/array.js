d7772c2f1f38da84d77e3566bd3d47d92c485cf8
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-04-23 14:23:34 +0200

    es-classes completed

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,13 @@
 // Your code here
+Array.prototype.isEqual = function(arr){
+    if(this.length !== arr.length){         //this current array instance
+        return false;
+    }
+
+    for(let i = 0; i < this.length; i++){
+        if(this[i] !== arr[i]){
+            return false;
+        }
+    }
+    return true
+}
